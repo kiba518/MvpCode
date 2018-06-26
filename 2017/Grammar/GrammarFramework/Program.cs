1255ad342279b1947b75fdcb71cbd88a6a043443
kiba518 <jinxuanfeng518@163.com>
2018-06-26 15:19:33 +0800

    tuple code
    tuple code

--- 2017/Grammar/GrammarFramework/Program.cs
+++ 2017/Grammar/GrammarFramework/Program.cs
@@ -1,25 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Grammar
+namespace GrammarFramework
 {
     class Program
     {
         static void Main(string[] args)
         {
-             
+            //目标框架.net framework 4.0 ，我们可以看到属性的声明：
+
             var tuple = new Tuple<string, int, int, int>(
-                           "Kiba", 00001, 00002,
-                           00003); 
-           
+                             "Kiba", 00001, 00002,
+                             00003);
+
             Console.WriteLine(tuple.Item1);
             Console.WriteLine(tuple.Item2);
             Console.WriteLine(tuple.Item3);
             Console.WriteLine(tuple.Item4);
 
             var tupleCalss = new Tuple<A, B>(
-                         new A(), new B() );
+                         new A(), new B());
             Console.WriteLine(tupleCalss.Item1.Name);
             Console.WriteLine(tupleCalss.Item2.Name);
+             
             Console.ReadKey();
         }
     }
